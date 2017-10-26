185b00a6ac6fd182cd755799260aaddcf92c6d2b
lly <bjlilingyu@corp.netease.com>
2017-10-26 18:24:59 +0800

    add

---
+++ LLYSDK/Classes/LLYSDKTest.h
@@ -0,0 +1,12 @@
+//
+//  LLYSDKTest.h
+//  Expecta
+//
+//  Created by lly on 2017/10/26.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLYSDKTest : NSObject
+
+@end
---
+++ LLYSDK/Classes/LLYSDKTest.m
@@ -0,0 +1,12 @@
+//
+//  LLYSDKTest.m
+//  Expecta
+//
+//  Created by lly on 2017/10/26.
+//
+
+#import "LLYSDKTest.h"
+
+@implementation LLYSDKTest
+
+@end
